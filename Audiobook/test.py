2bdea1898aa2e6a21a04c944505708ea874e5267
Aidanjm13 <ajmathu13@outlook.com>
2026-08-07 20:41:51 -0400

    Adding function to split text into sentences
    This will allow only full sentences to be put into the audiobook pages so that tts models work well

--- Audiobook/test.py
+++ Audiobook/test.py
@@ -5,19 +5,20 @@ import os
 
 #testing SQLAlchemy functions
 firstRow = get_book(1)
-print(firstRow.title)
-print(firstRow.author)
-print(firstRow.language)
-print(firstRow.last_accessed)
+#print(firstRow.title)
+#print(firstRow.author)
+#print(firstRow.language)
+#print(firstRow.last_accessed)
 
 
 
 
 # testing epub reader when in system
 
-# fileName = "pg84-images-3 - copy"
-# bookPath = os.path.join(getBooksFolderPath(), fileName, f"{fileName}.epub")
-# book = getBook(bookPath)
-# print(getCoverImagePath(book))
-# print(getCoverImage(book,getCoverImagePath(book)))
+fileName = "pg84-images-3 - copy"
+bookPath = os.path.join(getBooksFolderPath(), fileName, f"{fileName}.epub")
+book = getBook(bookPath)
+print(getChapterContent(book,3))
+#print(getCoverImagePath(book))
+#print(getCoverImage(book,getCoverImagePath(book)))
 
---
+++ Audiobook/textSplitting.py
@@ -0,0 +1,18 @@
+import re
+from dataclasses import dataclass, field
+from typing import List, Optional
+ 
+import pysbd
+ 
+#takes the text and splits it into sentences using pysbd
+_segmenter = pysbd.Segmenter(language="en", clean=False)
+def split_sentences(text: str, language: str = "en") -> List[str]:
+    text = text.strip()
+    if not text:
+        return []
+ 
+    segmenter = _segmenter if language == "en" else pysbd.Segmenter(
+        language=language, clean=False
+    )
+    sentences = [s.strip() for s in segmenter.segment(text)]
+    return [s for s in sentences if s]
